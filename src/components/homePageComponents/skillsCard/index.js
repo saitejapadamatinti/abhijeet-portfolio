31ab553a024afc2105bb8577bcb5a8325607510c
saitejapadamatinti <saitejapadamatinti1@gmail.com>
2023-07-15 09:42:44 +0530

    home animation speed checking

--- src/components/homePageComponents/skillsCard/index.js
+++ src/components/homePageComponents/skillsCard/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./index.css";
 
 const SkillCard = ({ skillName }) => {
+  console.log(skillName);
   return (
     <div className="skillcard-bg-image">
-        <div className="skil-card-head-div">
-      <h1 className="skill-card-main-text">{skillName}</h1>
+      <div className="skil-card-head-div">
+        <dvi className="skill-icon-div-bg">{skillName.skillIcon}</dvi>
+        <h1 className="skill-card-main-text">{skillName.skillName}</h1>
       </div>
     </div>
   );
