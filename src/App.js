fdbe22ffefd471538b23491fbdfaafbeb00277c4
saitejapadamatinti <115764964+saitejapadamatinti@users.noreply.github.com>
2023-07-17 07:07:55 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import BlogDetails from "./components/blogDetails";
 import BlogBaackendForm from "./adminComponent/blogAdminForm";
 import HomePage from "./components/HomePage";
 import UserEmailsAdmin from "./adminComponent/userEmailsAdmin";
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/blogs" element={<BlogPage />} />
-      <Route path="/blog/:id" element={<BlogDetails />} />
-      <Route path="/adminBlogForm" element={<BlogBaackendForm />} />
-      <Route path="/adminUsersEmails" element={<UserEmailsAdmin />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blogs" element={<BlogPage />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+        <Route path="/adminBlogForm" element={<BlogBaackendForm />} />
+        <Route path="/adminUsersEmails" element={<UserEmailsAdmin />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
